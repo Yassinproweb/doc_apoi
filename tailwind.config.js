b183983dbb1de18b159ab1af3df6085f40f8a4cd
Katungi Yassin <yassinkatungi67@gmail.com>
2025-10-05 11:37:19 +0300

    tailwind setup

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,23 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [],
+  content: [
+    "./views/**/*.html",
+    "./views/partials/**/*.html"
+  ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        w: '#FEFEFF', //-- white
+        f: '#EDEDF0', //-- off-white
+        d: '#1A1B23', //-- black
+        t: '#49433D', //-- text
+        b: '#387CFF', //-- blue
+      },
+      padding: {
+        x: '5vw',
+        y: '3.5rem',
+      },
+    },
   },
   plugins: [],
 }
-
