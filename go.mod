5493f5eefb5c7f9f7b79c278ce47dae9c99bb895
Katungi Yassin <yassinkatungi67@gmail.com>
2025-10-05 11:41:11 +0300

    main go file

--- go.mod
+++ go.mod
@@ -4,12 +4,15 @@ go 1.25.0
 
 require (
 	github.com/gofiber/fiber/v2 v2.52.9
+	github.com/gofiber/template/html/v2 v2.1.3
 	github.com/mattn/go-sqlite3 v1.14.32
 	golang.org/x/crypto v0.42.0
 )
 
 require (
 	github.com/andybalholm/brotli v1.1.0 // indirect
+	github.com/gofiber/template v1.8.3 // indirect
+	github.com/gofiber/utils v1.1.0 // indirect
 	github.com/google/uuid v1.6.0 // indirect
 	github.com/klauspost/compress v1.17.9 // indirect
 	github.com/mattn/go-colorable v0.1.13 // indirect
--- main.go
+++ main.go
@@ -1,1 +1,39 @@
 package main
+
+import (
+	"log"
+	"time"
+
+	"github.com/Yassinproweb/meet_doc.git/data"
+	"github.com/Yassinproweb/meet_doc.git/routes"
+	"github.com/gofiber/fiber/v2"
+	"github.com/gofiber/fiber/v2/middleware/session"
+	"github.com/gofiber/template/html/v2"
+)
+
+func main() {
+	// Initialize database
+	data.InitDB()
+	defer data.DB.Close()
+
+	// Fiber app with template engine
+	app := fiber.New(fiber.Config{
+		Views: html.New("./views", ".html"),
+	})
+
+	// Serve static files
+	app.Static("/static", "./static")
+
+	// Single session store for the whole app
+	s := session.New(session.Config{
+		Expiration:     24 * time.Hour,
+		CookieHTTPOnly: true,
+		CookieSecure:   false, // set to true when using HTTPS in production
+	})
+
+	// Routes (pass session store)
+	routes.DocRoutes(app, s)
+
+	// Start server
+	log.Fatal(app.Listen("0.0.0.0:4320"))
+}
